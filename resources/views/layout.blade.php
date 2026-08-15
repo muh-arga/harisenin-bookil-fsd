c3739c292553108943cd7457dd96264f97e383d2
Arga <arga.swara@beautyhaul.com>
2026-08-15 17:32:37 +0700

    feat: load jQuery from official CDN

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -35,6 +35,9 @@
         @yield('content')
     </div>
 
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+            integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+            crossorigin="anonymous"></script>
     <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     @yield('page-script')
 </body>
